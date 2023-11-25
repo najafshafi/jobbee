0f43eae55e1014cdbcd68536eb251418042e7878
GeeksEra <geeksera.online@gmail.com>
2023-11-25 23:22:07 +0400

    endpoint change

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 
-// export const BASE_API_URL = 'http://35.172.225.218:8080/v1';
-export const BASE_API_URL = 'http://localhost:8080/v1';
+export const BASE_API_URL = 'http://35.172.225.218:8080/v1';
+// export const BASE_API_URL = 'http://localhost:8080/v1';
 export const USER_TOKEN = 'USER_TOKEN';
 export const siteUrl = 'https://jobswitch.io/';
